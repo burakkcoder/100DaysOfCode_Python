d1e1ebf9979a0a91574cb16693ec432d57ee089b
burakkcoder <58417317+burakkcoder@users.noreply.github.com>
2021-11-30 09:37:28 +0300

    Delete Day 1 directory

--- Day 1/Exercise/exercise1.py
+++
@@ -1,4 +0,0 @@
-#Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
-
-isim = input("What is your name?")
-print(len(isim))
